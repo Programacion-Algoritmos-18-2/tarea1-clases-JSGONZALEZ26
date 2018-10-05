d620f5b6824f38d1a692f4e68c1e879cc1b31917
Jairo Gonzalez <jairo99gc@icloud.com>
2018-10-05 11:34:01 -0500

    Subiendo Tarea 1

---
+++ Semana_1_Clases/src/semana_1_clases/Estudiante.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana_1_clases;
+
+/**
+ *
+ * @author Jairo
+ */
+public class Estudiante {
+    public String nombre = "";
+    public String apellido = "";
+    public int edad = 0;
+    public String cedula = "";
+//Getters, Setters y Constructor
+    public Estudiante() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    
+    
+}
--- Semana_1_Clases/src/semana_1_clases/Semana_1_Clases.java
+++ Semana_1_Clases/src/semana_1_clases/Semana_1_Clases.java
@@ -5,6 +5,8 @@
  */
 package semana_1_clases;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Jairo
@@ -15,7 +17,37 @@ public class Semana_1_Clases {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+//Creacion de instancias
+        Estudiante estudiante1 = new Estudiante();
+        Estudiante estudiante2 = new Estudiante();
+        Scanner entrada = new Scanner(System.in);
+//Inicialización de Variables de la clase Main
+        int persona;
+        persona = 0;
+//Ingreso de Datos
+        System.out.println("Ingrese el nombre de la persona: 1");
+        estudiante1.nombre = entrada.nextLine();
+        System.out.println("Ingrese su apellido:");
+        estudiante1.apellido = entrada.nextLine();
+        System.out.println("Ingrese su cedula:");
+        estudiante1.cedula = entrada.nextLine();
+        System.out.println("Ingrese su edad");
+        estudiante1.edad = entrada.nextInt();
+        entrada.nextLine();
+        System.out.println("Ingrese el nombre de la persona: 2");
+        estudiante2.nombre = entrada.nextLine();
+        System.out.println("Ingrese su apellido:");
+        estudiante2.apellido = entrada.nextLine();
+        System.out.println("Ingrese su cedula:");
+        estudiante2.cedula = entrada.nextLine();
+        System.out.println("Ingrese su edad");
+        estudiante2.edad = entrada.nextInt();
+
+//Imprimir valores
+        System.out.printf("%s\t%s\t%-10s\t%s\n", "Nombre", "Apellido", "Cedula", "Edad");
+        System.out.printf("%s\t%-8s\t%-8s\t%d\n",estudiante1.nombre,estudiante1.apellido,estudiante1.cedula,estudiante1.edad);
+        System.out.printf("%s\t%-8s\t%-8s\t%d\n",estudiante2.nombre,estudiante2.apellido,estudiante2.cedula,estudiante2.edad);
+
     }
-    
+
 }
